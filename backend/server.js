ca0bcb7bc02946749eb1c810ce001d24ff9f505c
Roman Mirzoian <roman.mirzoian@playtomax.com>
2025-01-04 04:05:32 +0100

    feat: add deployment scripts for package file

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import { config } from "dotenv"; 
 import cookieParser from "cookie-parser";
@@ -11,11 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 config();
 const PORT = process.env.PORT || 5000;
-//const app = express();
 
-//app.get("/", (req, res) => {
-//    res.send("Hello");
-//});
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", 'dist', "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
